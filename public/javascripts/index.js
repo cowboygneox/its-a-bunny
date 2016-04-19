4f7356151f1b44e6ea71db95da7895ff34f35483
Sean Freitag <sean@gneoxsolutions.com>
2016-04-19 13:12:15 -0500

    Finishing the basic implementation of the web interface.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,76 @@
-function run(divId, wsUrl) {
-    console.log(wsUrl);
-    var webSocket = new WebSocket(wsUrl);
+var calculationHistory = [];
 
-    var history = [];
+function run(divId, expressionId, submitId, errorId, postUrl, wsUrl) {
+    var webSocket = new WebSocket(wsUrl);
 
     webSocket.onmessage = function (event) {
         var json = JSON.parse(event.data);
-        console.log(json);
         if (json.allHistory) {
-            history = json.allHistory;
-            console.log(json.allHistory);
-            $("#" + divId).html(JSON.stringify(history));
+            calculationHistory = json.allHistory;
+            $("#" + divId).html(formatHistoryTable());
+        } else if (json.history) {
+            if (calculationHistory.len > 9) {
+                calculationHistory = [json.history].concat(calculationHistory.arr.splice(calculationHistory.len - 2));
+            } else {
+                calculationHistory = [json.history].concat(calculationHistory);
+            }
+            console.log(calculationHistory);
+            $("#" + divId).html(formatHistoryTable());
         }
     }
+
+    $("#" + expressionId).focus(function(event) {
+        $("#" + errorId).html("");
+    });
+
+    function submit(event) {
+        var text = $("#" + expressionId).val();
+        var json = {expression: text}
+
+        function finishedPost(event) {
+            if (event.status != 202) {
+                $("#" + expressionId).blur();
+
+                $("#" + errorId).html(
+                    '<br />' +
+                    '<div class="panel panel-danger">' +
+                        '<div class="panel-heading">' +
+                            '<h3 class="panel-title">Evaluation Failed</h3>' +
+                        '</div>' +
+                        '<div class="panel-body">' + event.responseJSON.error + '</div>' +
+                    '</div>'
+                );
+            }
+        }
+
+        $.ajax({
+            type: "POST",
+            url:  postUrl,
+            data: JSON.stringify(json),
+            complete: finishedPost,
+            contentType: "application/json",
+            dataType: "json"
+        });
+    }
+
+    $("#" + submitId).click(submit);
+
+    $("form").keypress(function (e) {
+        if (e.which == 13) {
+            submit(e);
+            return false;
+        }
+    });
+}
+
+function formatHistoryTable() {
+    var rows = $.map(calculationHistory, function(history) {
+        return '<tr>' +
+            '<td>' + history.expression + ' = ' + history.result + '</td>' +
+        '</tr>';
+    });
+
+    return '<table class="table">' +
+        rows.join('') +
+    '</table>';
 }
